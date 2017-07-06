08494d4cc2061739f16fa3d0e3cc8ce9fe1ca518
Hannes Junnila <hannes.junnila@gmail.com>
2017-07-06 16:10:14 +0300

    add query cache

--- app/relayEnvironment.js
+++ app/relayEnvironment.js
@@ -3,6 +3,7 @@ import {
   Network,
   RecordSource,
   Store,
+  QueryResponseCache,
 } from 'relay-runtime';
 
 let environment;
@@ -13,21 +14,33 @@ const store = new Store(source);
 const handlerProvider = null;
 
 export function setNetworkLayerUrl(url) {
+  const cache = new QueryResponseCache({ size: 100, ttl: 10 * 60 * 1000 });
+
   function fetchQuery(
     operation,
     variables,
     ) {
+    const cachedResponse = cache.get(operation.name, variables);
+    if (cachedResponse) return Promise.resolve(cachedResponse);
+
     return fetch(url, {
       method: 'POST',
       headers: {
-        // Add authentication and other headers here
+          // Add authentication and other headers here
         'content-type': 'application/json',
       },
       body: JSON.stringify({
         query: operation.text.replace(/on Query/g, 'on QueryType'),
         variables,
       }),
-    }).then(response => response.json());
+    }).then(
+      response => response.json(),
+    ).then(
+      (response) => {
+        cache.set(operation.name, variables, response);
+        return response;
+      },
+    );
   }
 
   const network = Network.create(fetchQuery);
